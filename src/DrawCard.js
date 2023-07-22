6fcea4e6cadb995af5d904835b2d0301a495ca87
markpakingan <116990353+markpakingan@users.noreply.github.com>
2023-07-21 21:10:14 -0700

    added image & css

--- src/DrawCard.js
+++ src/DrawCard.js
@@ -1,25 +1,51 @@
 // DrawCard.js file
 
-import React, {useState, useEffect} from "react";
-import axios from "axios";
 
-const DrawCard = ({deckID}) => {
-    
-   useEffect(()=> {
-    axios.get(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=2`).then(
-        response => {
-            console.log(response.data.deckID);
-        }).catch(error => {
-            console.error(error);
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./Draw.css";
+
+const DrawCard = ({ deckID }) => {
+  const [cardImage, setCardImage] = useState(null);
+  const [cardCount, setCardCount] = useState(null);
+
+  useEffect(() => {
+    if (cardCount === null) {
+      axios
+        .get(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=2`)
+        .then((response) => {
+          console.log(response.data.deckID);
+          setCardCount(response.data.remaining);
+        })
+        .catch((error) => {
+          console.error(error);
         });
-   }, [deckID]);
- 
-    
-    return (
-        <div>
-            <button >Draw Card</button>
-        </div>
-    )
-}
+    }
+  }, [deckID, cardCount]);
+
+  const fetchCard = () => {
+    axios
+      .get(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`)
+      .then((response) => {
+        console.log(response.data.cards[0]);
+        console.log("Cards remaining", response.data.remaining);
+        setCardImage(response.data.cards[0].image);
+        setCardCount(response.data.remaining);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  return (
+    <div className="container">
+      {cardCount !== null && cardCount > 0 && (
+        <button onClick={fetchCard}>Draw Card</button>
+      )}
+      {cardImage && <img src={cardImage} alt="Drawn Card" />}
+      {cardCount !== null && <p>Cards remaining: {cardCount}</p>}
+    </div>
+  );
+};
 
 export default DrawCard;
